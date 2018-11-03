11868bd56cbe2742cde6ae169bbbdc77ad80cf43
kohei.iwamura <kohei.iwamura@iridge.jp>
2018-11-03 16:17:15 +0900

    develop onClick function of header_menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,18 @@
+import eventBus from '../../helper/eventBus'
+
 export default {
     name: 'Header',
+    data: function(){
+      return {
+        selectedMenu: "profile",
+        menus: ["profile", "work", "contact"]
+      }
+    },
+    methods: {
+      triggerChangeContent: function(menu) {
+        event.preventDefault();
+        this.selectedMenu = menu
+        eventBus.$emit('changeContent', menu);
+      }
+    }
 }
--- src/components/Top/Top.js
+++ src/components/Top/Top.js
@@ -1,7 +1,17 @@
 import Header from "../Header/index.vue"
 import Footer from "../Footer/index.vue"
+import eventBus from '../../helper/eventBus'
 
 export default {
     name: 'top',
-    components: { Header, Footer }
+    components: { Header, Footer },
+    mounted: function () {
+      eventBus.$on('changeContent', this.changeContent);
+    },
+    methods: {
+      changeContent: function (requreContet) {
+        console.log(requreContet)
+      }
+    }
+
 }
