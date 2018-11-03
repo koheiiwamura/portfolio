5e896ef610fcf8884939f0d89ea1ad7a53351a91
kohei.iwamura <kohei.iwamura@iridge.jp>
2018-11-03 17:30:52 +0900

    implement scroll of content when menu clicked

--- src/components/Top/Top.js
+++ src/components/Top/Top.js
@@ -2,15 +2,46 @@ import Header from "../Header/index.vue"
 import Footer from "../Footer/index.vue"
 import eventBus from '../../helper/eventBus'
 
+
+
 export default {
     name: 'top',
     components: { Header, Footer },
     mounted: function () {
       eventBus.$on('changeContent', this.changeContent);
     },
+    data: function () {
+      return {
+      }
+    },
     methods: {
-      changeContent: function (requreContet) {
-        console.log(requreContet)
+      changeContent: function (requreContent) {
+
+      var pageNum = 1
+      var pageNumDict = {
+        'profile': 0,
+        'work': 1,
+        'contact': 2
+      }
+
+      var slideCurrent = 0;
+      var slideWidth = $('.slide').outerWidth();
+      var slideNum = $('.slide').length;
+      var slideSetWidth = slideWidth * slideNum;
+
+      $('.slide-set').css('width', slideSetWidth);
+
+      var sliding = function(){
+        $('.slide-set').animate({
+          left: slideCurrent * -slideWidth
+        });
+      }
+
+      slideCurrent = pageNumDict[requreContent]
+      sliding()
+
+
+
       }
     }
 
