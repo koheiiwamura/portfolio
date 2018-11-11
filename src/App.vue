daa8aa67259de53d877b73f0b86e689e61242049
kohei.iwamura <kohei.iwamura@iridge.jp>
2018-11-11 18:04:43 +0900

    delete unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <router-view/>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
 </script>
 
 <style src="./assets/css/reset.css" />
