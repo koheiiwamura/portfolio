c8e0a50510a74ce2c35b0a70149a16b23033d4d0
kohei.iwamura <kohei.iwamura@iridge.jp>
2018-12-02 17:21:55 +0900

    set urls in vue-router and router-link

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,37 @@ import Top from '@/components/Top/index.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: '/',
   routes: [
     {
       path: '/',
       name: 'Top',
       component: Top
-    }
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Top
+    },
+    {
+      path: '/work',
+      name: 'Work',
+      component: Top,
+      children: [{
+          path: 'internship',
+          component: Top
+        },
+        {
+          path: 'iridge',
+          component: Top
+        }
+      ]
+    },
+    {
+      path: '/contact',
+      name: 'Contanct',
+      component: Top
+    },
   ]
 })
